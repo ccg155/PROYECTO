9bf68be2d1c0b6142b012241f771ac24afc459f3
Carlos Crespo Gutiérrez <ccg155@alu.ua.es>
2025-03-14 20:51:29 +0100

    characters - en progreso

---
+++ characters/character.py
@@ -0,0 +1,60 @@
+class Character:
+    """
+    Clase base para todos los personajes en el juego.
+    
+    Atributos:
+        name (str): El nombre del personaje.
+        health (int): Los puntos de vida del personaje.
+        attack_power (int): El poder de ataque base del personaje.
+        gold (int): La cantidad de oro que posee el personaje.
+        defense (int): La cantidad de defensa que posee el personaje.
+        xp (int): La cantidad de xp que posee el personaje.
+    """
+    def __init__(self, name: str, health: int = 100, max_health: int = 100, attack_power: int = 10, gold: int = 500, xp: int = 0, defense: int = 10, speed: int = 5, level: int = 1):
+        self.name = name
+        self.health = health
+        self.max_health = max_health
+        self.attack_power = attack_power
+        self.defense = defense
+        self.gold = gold
+        self.xp = xp
+        self.speed = 5
+        self.level = level
+    
+    def heal(self, amount: int):
+        if self.health + amount >= self.max_health:
+            self.health = self.max_health
+        else:
+            self.health += amount
+    
+    def take_damage(self, amount: int):
+        if self.health - amount < 0:
+            self.health = 0
+            print(f'{self.name} ha muerto.')
+        else:
+            self.health -= amount
+            print(f'{self.name} tiene ahora {self.health} puntos de vida.')
+    
+    def level_up(self):
+        self.level += 1
+        self.xp = 0
+    
+    def gain_xp(self, amount):
+        self.xp += amount
+        print(f'{self.name} ganó {amount} puntos de xp.')
+    
+    def attack(self, target: 'Character'):
+
+        target.take_damage(self.attack_power)
+        print(f'{self.name} atacó a {target}.')
+    
+    def is_alive(self) -> bool:
+        """
+        Returns:
+            bool: Devuelve True si el personaje esta vivo, Falso de lo contrario_
+        """
+        return self.health > 0
+    
+    
+
+        
---
+++ characters/player.py
@@ -0,0 +1,11 @@
+from characters import character
+
+class Paladin(Character):
+    """Clase para los personaje del tipo paladín.
+        Alta defensa, bajo ataque
+
+    Args:
+        Character (_type_): _description_
+    """
+   # def __init__(self, name: str, health: int, attack_power: int):
+        
